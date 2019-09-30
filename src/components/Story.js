5d1b8b6f4ab2dd3951e636f5c9cf236a19ea7d44
Sheilf <sheilf.code@gmail.com>
2019-09-30 01:16:17 -0700

    add folder structure

---
+++ src/components/Story.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import { getStory } from '../services/HackerNews'
+
+export const Story  = ({ storyID }) => {
+
+    const [story, setStory] = useState({});
+    useEffect(() => {
+        getStory(storyID).then(data => data && data.url && setStory(data))
+    }, []);
+
+    return story && story.url ? (
+        <>
+            <a href={story.url}><p>{story.title}</p></a>
+            By: <p>{story.by}</p>
+            Posted: <p>{story.time}</p>
+        </>
+    ) : null
+}
