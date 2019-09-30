9b2d50f4629023db5a91af728834b20ff59d238b
Sheilf <sheilf.code@gmail.com>
2019-09-30 03:43:29 -0700

    destructure getStory

--- src/services/HackerNews.js
+++ src/services/HackerNews.js
@@ -1,10 +1,15 @@
+import { selectFields } from '../utilities/selectFields';
+
 export const baseURL = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesURL = `${baseURL}newstories.json`;
 export const storyURL = `${baseURL}item/`;
 
+
 export const getStory = async (storyID) => {
     const result = await fetch(`${storyURL + storyID}.json`).then(data =>{
-        return data.json();
+       return data.json()
+    }).then(response =>{
+        return selectFields(response);
     }).catch(error=>{console.log(error)})
 
     return result;
